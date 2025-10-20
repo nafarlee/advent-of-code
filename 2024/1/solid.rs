a628bbec314c1f4a484d863593c698b2bbc307a6
Nicholas Farley <nicholasafarley@gmail.com>
2025-10-19 22:23:50 -0400

    Change solid Rust 2024/1 solution to use iterators more

--- 2024/1/solid.rs
+++ 2024/1/solid.rs
@@ -5,13 +5,13 @@ use std::iter::zip;
 fn main() {
     let filename = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(filename).unwrap();
-    let mut lefts: Vec<i32> = Vec::new();
-    let mut rights: Vec<i32> = Vec::new();
-    for line in contents.lines() {
-        let mut it = line.split_whitespace();
-        lefts.push(it.next().unwrap().parse().unwrap());
-        rights.push(it.next().unwrap().parse().unwrap());
-    }
+    let (mut lefts, mut rights): (Vec<i32>, Vec<i32>) = contents
+        .lines()
+        .map(|line| -> (i32, i32) {
+            let mut it = line.split_whitespace().map(|x| x.parse().unwrap());
+            (it.next().unwrap(), it.next().unwrap())
+        })
+        .unzip();
     lefts.sort();
     rights.sort();
     let sum: i32 = zip(lefts, rights).map(|(l, r)| (l - r).abs()).sum();
