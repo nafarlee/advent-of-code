2106a96d8f06785bc73e62e4876ac3bc49994dc3
Nicholas Farley <nicholasafarley@gmail.com>
2025-10-20 19:52:44 -0400

    Extract closures to top-level functions

--- 2024/1/rust/solid/src/main.rs
+++ 2024/1/rust/solid/src/main.rs
@@ -2,18 +2,21 @@ use std::env::args;
 use std::fs::read_to_string;
 use std::iter::zip;
 
+fn parse_line(line: &str) -> (i32, i32) {
+    let mut it = line.split_whitespace().map(|x| x.parse().unwrap());
+    (it.next().unwrap(), it.next().unwrap())
+}
+
+fn distance((x, y): (i32, i32)) -> i32 {
+    (x - y).abs()
+}
+
 fn main() {
     let filename = args().nth(1).unwrap();
     let contents = read_to_string(filename).unwrap();
-    let (mut lefts, mut rights): (Vec<i32>, Vec<i32>) = contents
-        .lines()
-        .map(|line| -> (i32, i32) {
-            let mut it = line.split_whitespace().map(|x| x.parse().unwrap());
-            (it.next().unwrap(), it.next().unwrap())
-        })
-        .unzip();
+    let (mut lefts, mut rights): (Vec<i32>, Vec<i32>) = contents.lines().map(parse_line).unzip();
     lefts.sort();
     rights.sort();
-    let sum: i32 = zip(lefts, rights).map(|(l, r)| (l - r).abs()).sum();
+    let sum: i32 = zip(lefts, rights).map(distance).sum();
     println!("{sum}");
 }
