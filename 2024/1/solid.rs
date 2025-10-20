69bfa6c76220265496a1e50746c3589e0f54d07a
Nicholas Farley <nicholasafarley@gmail.com>
2025-10-19 22:07:35 -0400

    Finish solid solution in Rust for 2024/1

--- 2024/1/solid.rs
+++ 2024/1/solid.rs
@@ -1,6 +1,19 @@
 use std::env;
+use std::fs;
+use std::iter::zip;
 
 fn main() {
-    let filename = env::args().nth(1).expect("Filename argument not provided");
-    println!("The filename is {}", filename);
+    let filename = env::args().nth(1).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+    let mut lefts: Vec<i32> = Vec::new();
+    let mut rights: Vec<i32> = Vec::new();
+    for line in contents.lines() {
+        let mut it = line.split_whitespace();
+        lefts.push(it.next().unwrap().parse().unwrap());
+        rights.push(it.next().unwrap().parse().unwrap());
+    }
+    lefts.sort();
+    rights.sort();
+    let sum: i32 = zip(lefts, rights).map(|(l, r)| (l - r).abs()).sum();
+    println!("{}", sum);
 }
